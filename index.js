cba6188d857e4d4cf4878bd03ecd038786d00dae
Mikheil17 <mikheil.gabrielidze@gmail.com>
2025-08-02 23:56:16 +0400

    adding glitch sound

--- index.js
+++ index.js
@@ -17,35 +17,42 @@ $(document).keydown(async function(event) {
 async function makeGuess(word) {
     const isValid = await checkDictionaryTrue(word);
 
-    if(isValid) {
+    if (isValid) {
 
-        if(word === randomWord) {
+        if (word === randomWord) {
             await correctLetters(word, randomWord);
-            if(row === 6 ) {
-                if(isComic) {showComicMessage("omg", 750);}
+            if (row === 6) {
+                if (isComic) {
+                    showComicMessage("omg", 750);
+                }
                 dance(row);
-            }
-            else {
+            } else {
                 let ran = Math.floor(Math.random() * 4 + 1);
-                if(isComic) {showComicMessage(`win${ran}`, 750);}
+                if (isComic) {
+                    showComicMessage(`win${ran}`, 750);
+                }
                 dance(row);
-                if(isComic) {$(".row" + row).children().addClass("explode");
-                await pause(300)
-                $(".row" + row).children().removeClass("explode");}
+                if (isComic) {
+                    $(".row" + row).children().addClass("explode");
+                    await pause(300)
+                    $(".row" + row).children().removeClass("explode");
+                }
 
-        
+
             }
-            if(isComic) {confettiExplosion(row);}
+            if (isComic) {
+                confettiExplosion(row);
+            }
             gameOver = true;
-        }
-        else {
-            if(row === 6) {
+        } else {
+            if (row === 6) {
                 await correctLetters(word, randomWord);
                 showMessage(randomWord);
-                if(isComic) {showComicMessage("zap", 750);}
+                if (isComic) {
+                    showComicMessage("zap", 750);
+                }
                 gameOver = true;
-            } 
-            else {
+            } else {
                 await correctLetters(word, randomWord);
                 row++;
                 index = 0;
@@ -58,48 +65,44 @@ async function makeGuess(word) {
 async function correctLetters(word, randomWord) {
     let greenLetters = {};
     let yellowLetters = {};
-    
+
     function countCharacters(str, char) {
         return str.split('').reduce(function(count, currentChar) {
             return currentChar === char ? count + 1 : count;
         }, 0);
     }
 
-    for(let i = 0; i < 5; i++) {
-        if(word[i] === randomWord[i]) {
+    for (let i = 0; i < 5; i++) {
+        if (word[i] === randomWord[i]) {
             greenLetters[word[i]] = (greenLetters[word[i]] || 0) + 1;
         }
     }
 
-    for(let i = 0; i < 5; i++) {
+    for (let i = 0; i < 5; i++) {
 
-        if(word[i] === randomWord[i]) {
+        if (word[i] === randomWord[i]) {
             $(".row" + row).children().eq(i).addClass("green").addClass("flipped").removeClass("frame").removeAttr("style").addClass("glossy").addClass("flipped-frame");
-        }
-        else if(randomWord.includes(word[i]) && countCharacters(randomWord, word[i]) > ((greenLetters[word[i]] || 0) + (yellowLetters[word[i]] || 0))) {
+        } else if (randomWord.includes(word[i]) && countCharacters(randomWord, word[i]) > ((greenLetters[word[i]] || 0) + (yellowLetters[word[i]] || 0))) {
             $(".row" + row).children().eq(i).addClass("yellow").addClass("flipped").removeClass("frame").removeAttr("style").addClass("glossy").addClass("flipped-frame");;
             yellowLetters[word[i]] = (yellowLetters[word[i]] || 0) + 1;
-        }
-        else {
+        } else {
             $(".row" + row).children().eq(i).addClass("gray").addClass("flipped").removeClass("frame").removeAttr("style").addClass("flipped-frame");
-        } 
+        }
 
         await pause(350);
         $(".row" + row).children().eq(i).removeClass("flipped");
 
     }
 
-    for(let i = 0; i < 5; i++) {
+    for (let i = 0; i < 5; i++) {
 
-        if(word[i] === randomWord[i]) {
+        if (word[i] === randomWord[i]) {
             pressKey(word[i], "green");
-        }
-        else if(randomWord.includes(word[i])) {
+        } else if (randomWord.includes(word[i])) {
             pressKey(word[i], "yellow");
-        }
-        else {
+        } else {
             pressKey(word[i], "gray");
-        } 
+        }
     }
 }
 
@@ -112,17 +115,20 @@ function popLetter(tile) {
 }
 
 function pause(milliseconds) {
-  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+    return new Promise((resolve) => setTimeout(resolve, milliseconds));
 }
 
 function showMessage(msg) {
-    if(isComic) {
-    $("#answer-message").text(msg).removeClass("hidden");
-    setTimeout(function() {$("#answer-message").addClass("hidden");}, 2000);
-    }
-    else {
+    if (isComic) {
+        $("#answer-message").text(msg).removeClass("hidden");
+        setTimeout(function() {
+            $("#answer-message").addClass("hidden");
+        }, 2000);
+    } else {
         $("#message").text(msg).removeClass("hidden");
-        setTimeout(function() {$("#message").addClass("hidden");}, 1000);
+        setTimeout(function() {
+            $("#message").addClass("hidden");
+        }, 1000);
     }
 }
 
@@ -131,14 +137,14 @@ function showComicMessage(word, duration) {
     const img = document.getElementById("msg-img");
     img.src = `./Images/explosions/${word}.png`;
     $("#message").removeClass("hidden");
-    setTimeout(function () {
+    setTimeout(function() {
         $("#message").addClass("hidden");
     }, duration);
 }
 
 
 async function dance(row) {
-    for(let i = 0; i < 5; i++) {
+    for (let i = 0; i < 5; i++) {
         $(".row" + row).children().eq(i).addClass("dance");
         await pause(100);
     }
@@ -150,13 +156,15 @@ $("button").click(function() {
 });
 
 async function inputLetter(key) {
-    if (gameOver || awaitingGuess || isInputLocked) {return;}
+    if (gameOver || awaitingGuess || isInputLocked) {
+        return;
+    }
 
-    if(key === "BACKSPACE" || key === "DELETE" || key === "⌫") {
-        if(index > 5) {
+    if (key === "BACKSPACE" || key === "DELETE" || key === "⌫") {
+        if (index > 5) {
             index = 5;
         }
-        if(index > 0) {
+        if (index > 0) {
             index--;
             chosenWordArray.pop();
             $(".row" + row).children().eq(index).html(``).removeClass("glossy").toggleClass("frame");
@@ -164,36 +172,40 @@ async function inputLetter(key) {
             $(".delete").addClass("pressed");
             isInputLocked = true;
             await pause(30);
-             isInputLocked = false;
+            isInputLocked = false;
             $(".delete").removeClass("pressed");
-            if(isComic) {new Audio("./Sounds/typing.mp3").play();}
+            if (isComic) {
+                new Audio("./Sounds/typing.mp3").play();
+            }
         };
-        
-    } 
-    else if(key === "ENTER"){
+
+    } else if (key === "ENTER") {
         $(".enter").addClass("pressed");
         isInputLocked = true;
         await pause(30);
         isInputLocked = false;
         $(".enter").removeClass("pressed");
-        if(isComic) {new Audio("./Sounds/typing.mp3").play();}
+        if (isComic) {
+            new Audio("./Sounds/typing.mp3").play();
+        }
 
-        if(index >= 5) {
-            
-            awaitingGuess =  true;
+        if (index >= 5) {
+
+            awaitingGuess = true;
             await makeGuess(chosenWordArray.join(""));
             awaitingGuess = false;
-        }
-        else {
-            if(isComic) {showComicMessage("oops", 500);} 
-            else {showMessage("Not enough letters")}
+        } else {
+            if (isComic) {
+                showComicMessage("oops", 500);
+            } else {
+                showMessage("Not enough letters")
+            }
 
             shake(row);
         }
-    }    
-    else if(/^[A-Z]$/.test(key) && index < 5) {
+    } else if (/^[A-Z]$/.test(key) && index < 5) {
 
-        const $button = $(`.key button`).filter(function () {
+        const $button = $(`.key button`).filter(function() {
             return $(this).text() === key;
         });
         const $keyDiv = $button.closest(".key");
@@ -204,40 +216,43 @@ async function inputLetter(key) {
 
         $(".row" + row).children().eq(index).html(`<p>${key}</p>`);
         popLetter($(".row" + row).children().eq(index));
-      
+
         index++;
         chosenWordArray.push(key);
-        if(isComic) {new Audio("./Sounds/typing.mp3").play();}
+        if (isComic) {
+            new Audio("./Sounds/typing.mp3").play();
+        }
     }
-    if(chosenWordArray.length > 5) {
-        chosenWordArray = chosenWordArray.slice( 0, 5);
+    if (chosenWordArray.length > 5) {
+        chosenWordArray = chosenWordArray.slice(0, 5);
     }
 }
 
 function pressKey(key, color) {
-    if(key !== "E") {
+    if (key !== "E") {
         $(`#game-board button:contains('${key}')`).addClass(color);
-    }
-    else {
+    } else {
         $(`#game-board k1 button:contains('${key}')`).addClass(color);
     }
 }
 
 async function checkDictionaryTrue(word) {
 
-    if(wordBank.includes(word.toLowerCase())){
+    if (wordBank.includes(word.toLowerCase())) {
         return true;
-    }
-    else {
-        if(isComic) {showComicMessage("smack", 500);}
-        else {showMessage("Not enough letters")}
+    } else {
+        if (isComic) {
+            showComicMessage("smack", 500);
+        } else {
+            showMessage("Not enough letters")
+        }
         shake(row);
         return false;
     }
 }
 
 async function shake(row) {
-    for(let i = 0; i < 5; i++) {
+    for (let i = 0; i < 5; i++) {
         $(".row" + row).children().eq(i).addClass("shake");
     }
 
@@ -245,11 +260,11 @@ async function shake(row) {
     $(".row" + row).children().removeClass("shake");
 }
 
-if(isComic) {
-    $('.tile').each(function () {
-      const randDeg = Math.floor(Math.random() * 360);
-      const filterVal = `hue-rotate(${randDeg}deg) saturate(3)`;
-      $(this).css('filter', filterVal);
+if (isComic) {
+    $('.tile').each(function() {
+        const randDeg = Math.floor(Math.random() * 360);
+        const filterVal = `hue-rotate(${randDeg}deg) saturate(3)`;
+        $(this).css('filter', filterVal);
     });
 }
 
@@ -258,63 +273,81 @@ function confettiExplosion(row) {
     const windowHeight = $(window).height();
     const originY = rowTop / windowHeight;
 
-  confetti({
-    particleCount: 150,
-    spread: 90,
-    origin: { y: originY * 2 } //change it so that height matches row
-  });
+    confetti({
+        particleCount: 150,
+        spread: 90,
+        origin: {
+            y: originY * 2
+        }
+    });
 }
 
-$("#reveal-button").click(function () {
-  if(isComic) {const sound = new Audio('./Sounds/button.mp3');
-  sound.currentTime = 0;
-  sound.play();}
+$("#reveal-button").click(function() {
+    if (isComic) {
+        const sound = new Audio('./Sounds/button.mp3');
+        sound.currentTime = 0;
+        sound.play();
+    }
 
-  $('h1').text(randomWord);
+    $('h1').text(randomWord);
 
-  setTimeout(function () {
-    $('h1').text('Wordle');
-  }, 3000);
+    setTimeout(function() {
+        $('h1').text('Wordle');
+    }, 1500);
 });
 
-$('#toggle-mode').change(function () {
-  const styleLink = document.getElementById("theme-style");
-  const sound = new Audio('./Sounds/switch.mp3');
-  sound.currentTime = 0;
-  sound.play();
+$('#toggle-mode').change(function() {
+    const styleLink = document.getElementById("theme-style");
+    const sound = new Audio('./Sounds/switch.mp3');
+    sound.currentTime = 0;
+    sound.play();
 
-  if (this.checked) {
-    styleLink.href = "comic.css";
-    isComic = true;
-  } else {
-    styleLink.href = "classic.css";
-    isComic = false;
-  }
+    if (this.checked) {
+        styleLink.href = "comic.css";
+        isComic = true;
+    } else {
+        styleLink.href = "classic.css";
+        isComic = false;
+    }
 
-  glitchEffect();
+    glitchEffect();
 });
 
 async function glitchEffect(duration = 200) {
-  const styleLink = document.getElementById("theme-style");
-  const originalHref = styleLink.getAttribute("href");
+    const styleLink = document.getElementById("theme-style");
+    const originalHref = styleLink.getAttribute("href");
 
-  let isComicTheme = originalHref.includes("comic.css");
-  let startTime = performance.now();
+    let isComicTheme = originalHref.includes("comic.css");
+    let startTime = performance.now();
 
-  $("body").addClass("glitchy glitch-overlay");
+    $("body").addClass("glitchy glitch-overlay");
 
-  const interval = setInterval(() => {
+    const glitchSounds = [
+        new Audio('./Sounds/glitch1.m4a'),
+        new Audio('./Sounds/glitch3.m4a')
+    ];
 
-    styleLink.href = isComicTheme ? "classic.css" : "comic.css";
-    isComicTheme = !isComicTheme;
-  }, 30);
+    glitchSounds.forEach(sound => {
+        sound.preload = "auto";
+        sound.load();
+    });
 
-  await pause(duration);
 
-  clearInterval(interval);
-  styleLink.href = originalHref;
+    const sound = glitchSounds[Math.floor(Math.random() * glitchSounds.length)];
+    sound.currentTime = 0;
+    sound.play().catch(() => {});
 
-  $("body").removeClass("glitchy glitch-overlay");
+
+    const interval = setInterval(() => {
+
+        styleLink.href = isComicTheme ? "classic.css" : "comic.css";
+        isComicTheme = !isComicTheme;
+    }, 30);
+
+    await pause(duration);
+
+    clearInterval(interval);
+    styleLink.href = originalHref;
+
+    $("body").removeClass("glitchy glitch-overlay");
 }
-
-//add glitch sound
